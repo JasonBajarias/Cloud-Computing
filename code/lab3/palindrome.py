9c917b4ede5d75ab15e3b023048c877045a1cd67
jasonbajarias <C13379416@mydit.ie>
2015-09-28 22:33:40 +0000

    ver 4

--- code/lab3/palindrome.py
+++ code/lab3/palindrome.py
@@ -1,11 +1,15 @@
 #this program check wether a string is a palindrome or not
-def Check_Palindrome(String):
-
+def Check_Palindrome(string):
+  check_string = string[::-1]
+  if (string == check_string):
+    return "String is a palindrome"
+  else:
+    return "String is not a palindrome"
 
 def main():
   string = input("Enter String: ")
   answer = Check_Palindrome(string)
-  print(asnwer)
-  
+  print(answer)
+
 if __name__ == '__main__':
   main()
