71acc97f1fff3c6e78414c5a3f94d31f2ed0a0e7
jasonbajarias <C13379416@mydit.ie>
2015-09-28 22:38:44 +0000

    Palindrome_Check Program

--- code/lab3/palindrome.py
+++ code/lab3/palindrome.py
@@ -1,15 +1,24 @@
 #this program check wether a string is a palindrome or not
+
+#function that checks if the string is a palindrome
 def Check_Palindrome(string):
+  #the string is sliced which reverses the string
   check_string = string[::-1]
+  #checks if the two strings are the same
   if (string == check_string):
     return "String is a palindrome"
   else:
     return "String is not a palindrome"
 
+#function main()
 def main():
+  #string is store in string
   string = input("Enter String: ")
+  #calling function Check_Palindrome
   answer = Check_Palindrome(string)
+  #prints the return from Check_Palindrome
   print(answer)
 
+#makes the program start at function main()
 if __name__ == '__main__':
   main()
